0790b58f6834ee4cae8e36be25716e5f80ee431d
Gio Meijer <meij0155@hz.nl>
2024-11-20 11:55:07 +0100

    mijn slechte rekenmachine

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,72 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Geen consistentie in de structuur of logica
+        System.out.println("Welkom bij de Rekenmachine, of zoiets...");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+        int x, y, z = 0; // Onnodige initialisatie en slechte naamgeving
+        System.out.println("Typ een random nummer:");
+        x = Integer.parseInt(sc.nextLine()); // Onnodig gebruik van parse zonder validatie
+
+        System.out.println("Typ nog een nummer:");
+        y = Integer.valueOf(sc.nextLine()); // Onnodig gebruik van waardeobject
+
+        // Willekeurige if-statements zonder reden
+        if (y > x) {
+            System.out.println("Dat is groot! Laten we beginnen.");
+        } else if (x == 0 || y == 0) {
+            System.out.println("We doen geen nullen hier. Maar oké.");
+        } else {
+            System.out.println("Misschien werkt het, misschien niet...");
+        }
+
+        // Alles in een gigantische switch-case, zonder consistentie
+        System.out.println("Typ 'plus', 'min', 'keer', of 'deel' om iets te doen:");
+        String actie = sc.nextLine();
+        switch (actie) {
+            case "plus":
+                z = x + y; // Magic Numbers en slechte variabele-namen
+                break;
+            case "min":
+                z = x - y;
+                break;
+            case "keer":
+                z = x * y;
+                break;
+            case "deel":
+                z = x / y; // Geen check voor deling door nul, leidt tot crash
+                break;
+            default:
+                System.out.println("Geen idee wat je bedoelt, maar we doen niks.");
+                z = -1; // Random waarde als fout
+                break;
+        }
+
+        // Debugcode laten staan in productie
+        System.out.println("DEBUG: x = " + x + ", y = " + y + ", z = " + z);
+
+        // Random string-output afhankelijk van z, geen reden voor deze logica
+        if (z == -1) {
+            System.out.println("Oeps, iets ging mis.");
+        } else if (z > 100) {
+            System.out.println("Wow, dat is groot! Resultaat: " + z);
+        } else {
+            System.out.println("Het resultaat is gewoon: " + z);
+        }
+
+        // Oneindige lus als grapje
+        while (z == -1) {
+            System.out.println("Haha, oneindige lus! Typ iets om te ontsnappen:");
+            if (sc.nextLine().equals("stop")) break; // Break uit lus met magische string
+        }
+
+        // Onnodige try-catch die niks doet
+        try {
+            System.out.println("Einde programma!");
+        } catch (Exception e) {
+            System.out.println("Dit gebeurt nooit.");
         }
     }
 }
